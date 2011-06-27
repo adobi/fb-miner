ded983db3a73136480f70bfa8e28df6ca451915a
Attila Dobi <hello@adobi.hu>
2011-06-27 21:07:26 +0200

    playing

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/invictus/game/app/public/';
+$config['base_url']	= 'http://fb-miner.orchestra.io';
 
 /*
 |--------------------------------------------------------------------------
